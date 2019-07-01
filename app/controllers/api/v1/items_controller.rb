56e44942701258b514b295c980230b06bb5d8cf0
kaelaclement <stephanie.kaela@gmail.com>
2019-07-01 16:06:22 -0700

    build items controller

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    @items = Item.all
+
+    render json: @items
+  end
+
+  def show
+    render json: @item
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+  private
+    def set_item
+      @item = Item.find_by(id: params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :url, :description, :price)
+    end
+end
