017d2144c3051bcb85e8c81a558ffd4c03fef04d
kaelaclement <stephanie.kaela@gmail.com>
2019-07-03 21:07:43 -0700

    display fetched items

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { getItems } from '../actions/items';
 
-export default class List extends Component {
+class List extends Component {
+
+  componentDidMount() {
+    this.props.getItems();
+  }
 
   render() {
-    return <h1>I'm in List!</h1>
+    return (
+      <ul>
+        { this.props.items.map(item => <li key={item.id}>{item.name}</li>) }
+      </ul>
+    )
   }
 
 }
+
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getItems: () => dispatch(getItems())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
