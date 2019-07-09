082cce7ea441ef20a23b41556b30f2f5989d97b6
kaelaclement <stephanie.kaela@gmail.com>
2019-07-08 17:25:36 -0700

    create route for new item form

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import { connect } from 'react-redux';
 import Item from './components/Item';
 import { getItems } from './actions/items';
+import ItemForm from './components/ItemForm';
 
 class App extends React.Component {
 
@@ -22,6 +23,7 @@ class App extends React.Component {
         </header> */}
         <Route exact path="/" component={ Home } />
         <Route exact path="/my_list" component={ List } />
+        <Route exact path="/new" component={ItemForm} />
         <Route exact path="/my_list/:itemId" render={ props => {
           const item = items.find(item => item.id.toString() === props.match.params.itemId)
           return <Item {...item} />
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { connect } from 'react-redux';
 import ItemPreview from './ItemPreview';
-import ItemForm from './ItemForm';
+import { Link } from 'react-router-dom';
 
 class List extends React.Component {
   render() {
     return (
       <div>
         
-        <ItemForm />
+        <Link to="/new">Add an item to your wish list</Link>
 
         <ul>
           { this.props.items.map(item => <ItemPreview key={item.id} name={item.name} id={item.id} />) }
