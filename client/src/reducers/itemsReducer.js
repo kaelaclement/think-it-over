61b4c9ca910991020746bca6a6a3c6a56fe9d68c
kaelaclement <stephanie.kaela@gmail.com>
2019-07-10 20:29:45 -0700

    add delete item to items reducer

--- client/src/reducers/itemsReducer.js
+++ client/src/reducers/itemsReducer.js
@@ -8,6 +8,8 @@ export default function itemsReducer(
         return { items: action.payload }
       case 'ADD_ITEM':
         return { items: state.items.concat(action.payload) }
+      case 'DELETE_ITEM':
+        return { items: state.items.filter(item => item.id !== action.payload)}
       default:
         return state;
     }
