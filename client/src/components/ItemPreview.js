1f1d1d31c0e15528fabd499d58cf667395cb0fbd
kaelaclement <stephanie.kaela@gmail.com>
2019-07-19 20:43:36 -0700

    install react-bootstrap

--- client/src/components/ItemPreview.js
+++ client/src/components/ItemPreview.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const ItemPreview = props => {
-  return <li><button onClick={() => props.handleClick(props.id)}>X</button><Link to={`/my_list/${props.id}`}>{ props.name }</Link></li>
+  return <li><Button variant="primary" onClick={() => props.handleClick(props.id)}>X</Button><Link to={`/my_list/${props.id}`}>{ props.name }</Link></li>
 }
 
 export default ItemPreview;
