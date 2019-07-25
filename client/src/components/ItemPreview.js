23f41c1a137eb9ee2ea5e8e6fcc7c70c5418daa0
kaelaclement <stephanie.kaela@gmail.com>
2019-07-24 22:30:25 -0700

    style ItemPreview

--- client/src/components/ItemPreview.js
+++ client/src/components/ItemPreview.js
@@ -4,8 +4,8 @@ import Button from 'react-bootstrap/Button';
 
 const ItemPreview = props => {
   return (
-    <div className="p-2">
-      <Button className="p-2" variant="outline-danger" size="sm" onClick={() => props.handleClick(props.id)}>x</Button>
+    <div className="p-3">
+      <Button className="mr-3" variant="outline-danger" size="sm" onClick={() => props.handleClick(props.id)}>x</Button>
       <Link to={`/my_list/${props.id}`}>{ props.name }</Link>
     </div>
   )
