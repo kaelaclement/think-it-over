1ad6fd4a89753d05044ca27321ede4b25e73940c
kaelaclement <stephanie.kaela@gmail.com>
2019-07-25 23:04:17 -0700

    style Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
+    <main className="mt-3">
       <h1>Welcome to Think It Over!</h1>
+      <p>It can be all too easy to buy with one click only to regret it later.</p>
       <p>Find an item you like? Put it on your wish list, give it a week, then come back!</p>
-      <Link to="/my_list">Head to your list</Link>
-    </>
+      <p>Use navigation above to get started</p>
+    </main>
   )
 }
 
