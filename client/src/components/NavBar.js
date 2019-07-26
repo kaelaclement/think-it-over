af02c5e4122993ffe8586bdaa6f666a32758d2b6
kaelaclement <stephanie.kaela@gmail.com>
2019-07-25 23:04:03 -0700

    switch list and add item links in NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,8 +7,8 @@ const NavBar = () => {
     <Navbar sticky="top" className="justify-content-around p-3" style={{background: '#bce6b4'}}>
       <Navbar.Brand href='/'>Think It Over</Navbar.Brand>
       <Nav>
-        <Nav.Link href='/new'>Add an Item</Nav.Link>
         <Nav.Link href='/my_list'>My List</Nav.Link>
+        <Nav.Link href='/new'>Add an Item</Nav.Link>
       </Nav>
     </Navbar>
   )
