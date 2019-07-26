1f6076cbcd26d4d93823c05dba9880c65f3eb79a
kaelaclement <stephanie.kaela@gmail.com>
2019-07-26 16:03:06 -0700

    open buy link in new tab

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -6,7 +6,7 @@ const Item = props => {
       <h1>{props.name}</h1>
       <p>{props.description}</p>
       <p>{props.price}</p>
-      <a href={props.url}>Buy</a><br />
+      <a target="_blank" rel="noopener noreferrer" href={props.url}>Buy</a><br />
     </div>
   )
 }
